cd59e68b79b5ad407cf034074270e7334d14aa14
joelprakash <joelprakash703@gmail.com>
2017-03-06 11:47:35 -0500

    Basic AMA Form Model (#7)
    *Basic AMA Form Model
    *Implemented the basic AMA form Model as a SpringBoot and Maven Project
    *Added pom.xml file
    *3 Classes (User,AMA and Application)
    *Initialised List<AMA> and List<String>

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,16 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by joelprakash on 3/5/2017.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+

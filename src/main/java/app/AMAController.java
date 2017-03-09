bfa71bbeac093e3bd058206700a61eb7c4492275
adhichakraborty <adhichakraborty@172.20.10.2>
2017-03-09 10:08:48 -0500

    temp

--- src/main/java/app/AMAController.java
+++ src/main/java/app/AMAController.java
@@ -3,9 +3,11 @@ package app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 public class AMAController {
@@ -15,22 +17,19 @@ public class AMAController {
 	 @Autowired
 	 UserRepository use;
 	 
-	@PostMapping("/user/AMA/AMA-Creation")
-	public String createAMA(@RequestParam(value="name") String name,@RequestParam(value="handle") String handle,
+	@PostMapping("{handle}/AMA/AMA-Creation")
+	public @ResponseBody String createAMA(@PathVariable("handle") String handle, @RequestParam(value="name") String name,
 			@RequestParam(value="deadline to vote") String date,
 			@RequestParam(value="description") String description, @RequestParam(value="Key words") String tags, Model m){
-		User user=new User(name, handle);
-		use.save(user);
 		AMA amamade=new AMA(description);
 		amamade.setListOfKeyWords(amamade.getKeyWords(tags));
-		amamade.setCreator(user);
 		amamade.setDeadlineToVote(amamade.convertStringToDate(date));
 		ama.save(amamade);
 		m.addAttribute("ama");
 		return "ama/display/ID";
 	}
-	@GetMapping("/user/AMA/ID")
-	public String displayAMA(@RequestParam(value="ID") long ID, Model m){
+	@GetMapping("{handle}/user/AMA/{ID}")
+	public @ResponseBody String displayAMA(@PathVariable("handle") String handle, @PathVariable("ID") Long ID, Model m){
 		AMA wantedama=ama.findOne(ID);
 		m.addAttribute("wantedAMA");
 		return "ama/display/ID";

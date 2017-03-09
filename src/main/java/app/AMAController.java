b4a3ea10ac0cf08a10c249f4639ec3b6bb53e8a4
jonathanscothorn <scothorn.jonathan@gmail.com>
2017-03-09 12:33:24 -0500

    view added

--- src/main/java/app/AMAController.java
+++ src/main/java/app/AMAController.java
@@ -1,39 +1,34 @@
 package app;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+
+@Controller
 public class AMAController {
-	 @Autowired
-	 AMARepository ama;
+
+	@Autowired
+	 AMARepository amaR;
 	 
 	 @Autowired
 	 UserRepository use;
-	 
-	@PostMapping("/user/AMA/AMA-Creation")
-	public String createAMA(@RequestParam(value="name") String name,@RequestParam(value="handle") String handle,
-			@RequestParam(value="deadline to vote") String date,
-			@RequestParam(value="description") String description, @RequestParam(value="Key words") String tags, Model m){
-		User user=new User(name, handle);
-		use.save(user);
-		AMA amamade=new AMA(description);
-		amamade.setListOfKeyWords(amamade.getKeyWords(tags));
-		amamade.setCreator(user);
-		amamade.setDeadlineToVote(amamade.convertStringToDate(date));
-		ama.save(amamade);
-		m.addAttribute("ama");
-		return "ama/display/ID";
-	}
-	@GetMapping("/user/AMA/ID")
-	public String displayAMA(@RequestParam(value="ID") long ID, Model m){
-		AMA wantedama=ama.findOne(ID);
-		m.addAttribute("wantedAMA");
-		return "ama/display/ID";
+	//@PathVariable("handle") String handle
+
+	@PostMapping("/ama")
+	public String createAMA(@ModelAttribute("ama") AMA ama){
+		//amamade.setListOfKeyWords(amamade.getKeyWords(tags));
+		amaR.save(ama);
+		return "user";
+	}//@PathVariable("handle") String handle
+
+
+	@GetMapping("/ama")
+	public String displayAMA(Model model) {
+		model.addAttribute("ama", new AMA());
+		return "ama";
 	}
 	
 }

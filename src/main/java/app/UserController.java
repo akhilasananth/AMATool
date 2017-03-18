9a31b1f61000dfe0465a51aacea1c5ab6b2a9e8d
joelprakash <joelprakash703@gmail.com>
2017-03-17 21:36:37 -0400

    Creating the User Controller and Repo

---
+++ src/main/java/app/UserController.java
@@ -0,0 +1,34 @@
+package app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * Created by joelprakash on 3/17/2017.
+ */
+@Controller
+public class UserController {
+
+    @Autowired
+    UserRepository useR;
+    //@PathVariable("handle") String handle
+
+
+    @PostMapping("/user-creation")
+    public String createAMA(@ModelAttribute("user-creation") User user){
+        //amamade.setListOfKeyWords(amamade.getKeyWords(tags));
+        useR.save(user);
+        return "user-creation";
+    }//@PathVariable("handle") String handle
+
+
+    @GetMapping("/user-creation")
+    public String displayAMA(Model model) {
+        model.addAttribute("user", new User());
+        return "user-creation";
+    }
+}

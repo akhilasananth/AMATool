4e45ff7cbc0200ec54432d24dbc9d7f9c6bbf514
adhirajC <kingadhi@hotmail.com>
2017-04-02 14:25:36 -0400

    User ama link bug fix (#58)
    * After Ama is created, the link the create another AMA works
    
    * Created AMA is saved to the user! :)
    
    * -fixed the AMA being assigned to a certain user bug, also removed submit another AMA option for now
    
    * took care of bug that saves AMA to all users

--- src/main/java/app/AMAController.java
+++ src/main/java/app/AMAController.java
@@ -57,13 +57,14 @@ public class AMAController {
 		//	amasList.add(ama);
 		//}
 
+		handle=new String(userhandle);
+
         user =useR.findByHandle(handle);
         displayAMAID=user.getId();
 		ListIterator<AMA> listIterator = amaR.findAllByCreatorID(displayAMAID).listIterator();
 		while(listIterator.hasNext()){
 			amasList.add(listIterator.next());
 		}
-		model.addAttribute("ama", new AMA());
         model.addAttribute("user",user);
 		model.addAttribute("amasList",amasList);
 		return "displayAMAsByUser";
